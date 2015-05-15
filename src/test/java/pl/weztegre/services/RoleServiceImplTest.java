b95913a94c902b2e441951ed6e0e833bf07837d9
st3rn <st3rnik@gmail.com>
2015-05-15 14:53:48 +0200

    RoleServiceImpl Testy

---
+++ src/test/java/pl/weztegre/services/RoleServiceImplTest.java
@@ -0,0 +1,36 @@
+package pl.weztegre.services;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.runners.MockitoJUnitRunner;
+import pl.weztegre.controllers.RegistrationController;
+import pl.weztegre.models.Role;
+import pl.weztegre.repositories.RoleRepository;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by st3rn on 15.05.15.
+ */
+@RunWith(MockitoJUnitRunner.class)
+public class RoleServiceImplTest {
+
+    @InjectMocks private RoleServiceImpl roleServiceImpl;
+    @Mock private RoleRepository roleRepositoryMock;
+
+    @Test
+    public void testFindRole() throws Exception {
+        roleServiceImpl.findRole("role");
+        Mockito.verify(roleRepositoryMock).findByRole("role");  //sprawdź czy hasErrors() zostało wywołane
+    }
+
+    @Test
+    public void testCreateRole() throws Exception {
+        roleServiceImpl.createRole("role");
+        Mockito.verify(roleRepositoryMock).save(Mockito.any(Role.class));  //sprawdź czy hasErrors() zostało wywołane
+
+    }
+}
